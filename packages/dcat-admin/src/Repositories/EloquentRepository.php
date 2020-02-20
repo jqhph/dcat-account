77fec075915ad313c1712393803bcddc78abd4c7
jqh <841324345@qq.com>
2020-02-20 15:27:47 +0800

    update

--- packages/dcat-admin/src/Repositories/EloquentRepository.php
+++ packages/dcat-admin/src/Repositories/EloquentRepository.php
@@ -18,8 +18,6 @@ use Spatie\EloquentSortable\Sortable;
 
 class EloquentRepository extends Repository implements TreeRepository
 {
-    use HasTree;
-
     /**
      * @var string
      */
@@ -391,6 +389,69 @@ class EloquentRepository extends Repository implements TreeRepository
                 $this->getFormColumns()
             )
             ->toArray();
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentColumn()
+    {
+        return $this->eloquent()->getParentColumn();
+    }
+
+    /**
+     * Get title column.
+     *
+     * @return string
+     */
+    public function getTitleColumn()
+    {
+        return $this->eloquent()->getTitleColumn();
+    }
+
+    /**
+     * Get order column name.
+     *
+     * @return string
+     */
+    public function getOrderColumn()
+    {
+        return $this->eloquent()->getOrderColumn();
+    }
+
+    /**
+     * Save tree order from a tree like array.
+     *
+     * @param array $tree
+     * @param int   $parentId
+     */
+    public function saveOrder($tree = [], $parentId = 0)
+    {
+        $this->eloquent()->saveOrder($tree, $parentId);
+    }
+
+    /**
+     * Set query callback to model.
+     *
+     * @param \Closure|null $query
+     *
+     * @return $this
+     */
+    public function withQuery($queryCallback)
+    {
+        $this->eloquent()->withQuery($queryCallback);
+
+        return $this;
+    }
+
+    /**
+     * Format data to tree like array.
+     *
+     * @return array
+     */
+    public function toTree()
+    {
+        return $this->eloquent()->toTree();
     }
 
     /**
